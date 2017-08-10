11b42e9f3d6161c50abeb5ae58d4023aa9352b08
dylanling <dylan.ling@lendup.com>
2017-08-09 18:12:40 -0700

    make integer value readonly

--- src/sim/math/integer.ts
+++ src/sim/math/integer.ts
@@ -1,41 +1,39 @@
 export class Integer {
-  private _value: number;
+  readonly value: number;
 
   private constructor(value: number) {
-    this._value = Math.round(value);
+    this.value = Math.round(value);
   }
 
-  value = (): number => this._value;
-
   plus = (other: Integer): Integer => 
-    Integer.from(this.value() + other.value());
+    Integer.from(this.value + other.value);
 
   minus = (other: Integer): Integer => 
-    Integer.from(this.value() - other.value());
+    Integer.from(this.value - other.value);
 
   times = (other: Integer): Integer => 
-    Integer.from(this.value() * other.value());
+    Integer.from(this.value * other.value);
 
   dividedBy = (other: Integer): Integer => 
-    Integer.from(Math.trunc(this.value() / other.value()));
+    Integer.from(Math.trunc(this.value / other.value));
 
   roundUpDividedBy = (other: Integer): Integer => 
     this.plus(other).minus(Integer.from(1)).dividedBy(other);
 
   equals = (other: Integer): boolean =>
-    this.value() === other.value();
+    this.value === other.value;
 
   greaterThan = (other: Integer): boolean =>
-    this.value() > other.value();
+    this.value > other.value;
   
   greaterThanOrEqualTo = (other: Integer): boolean =>
-    this.value() >= other.value();
+    this.value >= other.value;
   
   lessThan = (other: Integer): boolean =>
-    this.value() < other.value();
+    this.value < other.value;
 
   lessThanOrEqualTo = (other: Integer): boolean =>
-    this.value() <= other.value();
+    this.value <= other.value;
 
   static from = (number: number): Integer =>
     new Integer(number);
@@ -47,7 +45,7 @@ export class Integer {
     a.lessThanOrEqualTo(b) ? a : b;
 
   static random = (min: Integer, max: Integer): Integer =>
-    Integer.from(Math.floor(Math.random() * (max.value() - min.value() + 1)) + min.value());
+    Integer.from(Math.floor(Math.random() * (max.value - min.value + 1)) + min.value);
 
   static readonly MIN = Integer.from(-9999);
   static readonly MAX = Integer.from(9999);
